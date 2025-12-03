48c2a28b786bb9e036297cb541369e021facd41b
Diego Augusto <101666111+diegoaugusto7@users.noreply.github.com>
2025-12-03 17:37:37 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Ana Carolina, eu amo você. Se você me ama me manda um emoji de Pêra 🍐.</h1>
+    return <h1>Ana Carolina, eu amo você. Se você me ama me manda um emoji de Pêra 🍐. kkkk </h1>
 }
 
 export default Home;
